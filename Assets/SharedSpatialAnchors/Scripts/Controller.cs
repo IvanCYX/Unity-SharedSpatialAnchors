ac044aeda6706f2ddbd3440be3d779ad50e92e52
Ivan Cheah <ixian2@huskers.unl.edu>
2024-02-19 14:27:22 -0600

    Manager Script Changes

--- Assets/SharedSpatialAnchors/Scripts/Controller.cs
+++ Assets/SharedSpatialAnchors/Scripts/Controller.cs
@@ -7,34 +7,34 @@ public class Controller : MonoBehaviour
     private float coordIncrement = 0.2f;
     private float rotIncrement = 1f;
     
-    public void DecreaseX() { DemoManager.MagnetScene.transform.position = new Vector3(DemoManager.MagnetScene.transform.position.x - coordIncrement, DemoManager.MagnetScene.transform.position.y, DemoManager.MagnetScene.transform.position.z); }
-    public void DecreaseY() { DemoManager.MagnetScene.transform.position = new Vector3(DemoManager.MagnetScene.transform.position.x, DemoManager.MagnetScene.transform.position.y - coordIncrement, DemoManager.MagnetScene.transform.position.z); }
-    public void DecreaseZ() { DemoManager.MagnetScene.transform.position = new Vector3(DemoManager.MagnetScene.transform.position.x, DemoManager.MagnetScene.transform.position.y, DemoManager.MagnetScene.transform.position.z - coordIncrement); }
-    public void DecreaseXRot() { DemoManager.MagnetScene.transform.rotation = DemoManager.MagnetScene.transform.rotation * Quaternion.AngleAxis(-rotIncrement, DemoManager.MagnetScene.transform.right); }
-    public void DecreaseYRot() { DemoManager.MagnetScene.transform.rotation = DemoManager.MagnetScene.transform.rotation * Quaternion.AngleAxis(-rotIncrement, DemoManager.MagnetScene.transform.up); }
-    public void DecreaseZRot() { DemoManager.MagnetScene.transform.rotation = DemoManager.MagnetScene.transform.rotation * Quaternion.AngleAxis(-rotIncrement, DemoManager.MagnetScene.transform.forward); }
+    public void DecreaseX() { ObjectManager.MagnetScene.transform.position = new Vector3(ObjectManager.MagnetScene.transform.position.x - coordIncrement, ObjectManager.MagnetScene.transform.position.y, ObjectManager.MagnetScene.transform.position.z); }
+    public void DecreaseY() { ObjectManager.MagnetScene.transform.position = new Vector3(ObjectManager.MagnetScene.transform.position.x, ObjectManager.MagnetScene.transform.position.y - coordIncrement, ObjectManager.MagnetScene.transform.position.z); }
+    public void DecreaseZ() { ObjectManager.MagnetScene.transform.position = new Vector3(ObjectManager.MagnetScene.transform.position.x, ObjectManager.MagnetScene.transform.position.y, ObjectManager.MagnetScene.transform.position.z - coordIncrement); }
+    public void DecreaseXRot() { ObjectManager.MagnetScene.transform.rotation = ObjectManager.MagnetScene.transform.rotation * Quaternion.AngleAxis(-rotIncrement, ObjectManager.MagnetScene.transform.right); }
+    public void DecreaseYRot() { ObjectManager.MagnetScene.transform.rotation = ObjectManager.MagnetScene.transform.rotation * Quaternion.AngleAxis(-rotIncrement, ObjectManager.MagnetScene.transform.up); }
+    public void DecreaseZRot() { ObjectManager.MagnetScene.transform.rotation = ObjectManager.MagnetScene.transform.rotation * Quaternion.AngleAxis(-rotIncrement, ObjectManager.MagnetScene.transform.forward); }
 
-    public void IncreaseX() { DemoManager.MagnetScene.transform.position = new Vector3(DemoManager.MagnetScene.transform.position.x + coordIncrement, DemoManager.MagnetScene.transform.position.y, DemoManager.MagnetScene.transform.position.z); }
-    public void IncreaseY() { DemoManager.MagnetScene.transform.position = new Vector3(DemoManager.MagnetScene.transform.position.x, DemoManager.MagnetScene.transform.position.y + coordIncrement, DemoManager.MagnetScene.transform.position.z); }
-    public void IncreaseZ() { DemoManager.MagnetScene.transform.position = new Vector3(DemoManager.MagnetScene.transform.position.x, DemoManager.MagnetScene.transform.position.y, DemoManager.MagnetScene.transform.position.z + coordIncrement); }
-    public void IncreaseXRot() { DemoManager.MagnetScene.transform.rotation = DemoManager.MagnetScene.transform.rotation * Quaternion.AngleAxis(rotIncrement, DemoManager.MagnetScene.transform.right); }
-    public void IncreaseYRot() { DemoManager.MagnetScene.transform.rotation = DemoManager.MagnetScene.transform.rotation * Quaternion.AngleAxis(rotIncrement, DemoManager.MagnetScene.transform.up); }
-    public void IncreaseZRot() { DemoManager.MagnetScene.transform.rotation = DemoManager.MagnetScene.transform.rotation * Quaternion.AngleAxis(rotIncrement, DemoManager.MagnetScene.transform.forward); }
+    public void IncreaseX() { ObjectManager.MagnetScene.transform.position = new Vector3(ObjectManager.MagnetScene.transform.position.x + coordIncrement, ObjectManager.MagnetScene.transform.position.y, ObjectManager.MagnetScene.transform.position.z); }
+    public void IncreaseY() { ObjectManager.MagnetScene.transform.position = new Vector3(ObjectManager.MagnetScene.transform.position.x, ObjectManager.MagnetScene.transform.position.y + coordIncrement, ObjectManager.MagnetScene.transform.position.z); }
+    public void IncreaseZ() { ObjectManager.MagnetScene.transform.position = new Vector3(ObjectManager.MagnetScene.transform.position.x, ObjectManager.MagnetScene.transform.position.y, ObjectManager.MagnetScene.transform.position.z + coordIncrement); }
+    public void IncreaseXRot() { ObjectManager.MagnetScene.transform.rotation = ObjectManager.MagnetScene.transform.rotation * Quaternion.AngleAxis(rotIncrement, ObjectManager.MagnetScene.transform.right); }
+    public void IncreaseYRot() { ObjectManager.MagnetScene.transform.rotation = ObjectManager.MagnetScene.transform.rotation * Quaternion.AngleAxis(rotIncrement, ObjectManager.MagnetScene.transform.up); }
+    public void IncreaseZRot() { ObjectManager.MagnetScene.transform.rotation = ObjectManager.MagnetScene.transform.rotation * Quaternion.AngleAxis(rotIncrement, ObjectManager.MagnetScene.transform.forward); }
 
     public void SpawnNewMagnet()
     {
-        GameObject newMagnet = Instantiate(DemoManager.LargeMagnet, gameObject.transform.position, Quaternion.identity);
+        GameObject newMagnet = Instantiate(ObjectManager.LargeMagnet, gameObject.transform.position, Quaternion.identity);
         Magnet magnet = newMagnet.GetComponent<Magnet>();
         Debug.Log("Spawned Magnet");
     }
 
     public void HideModel()
     {
-        DemoManager.ironFilling.SetActive(false);
+        ObjectManager.ironFilling.SetActive(false);
     }
     public void ShowModel()
     {
-        DemoManager.ironFilling.SetActive(true);
+        ObjectManager.ironFilling.SetActive(true);
     }
 
     void CheckInput()
